b75937b2b9d9112aa32ff67bbe9ec23a284016f0
NikaN <NikaN7602@gmail.com>
2024-04-16 18:12:00 +0200

    README.md

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,5 @@
 import createPopover from './form.js';
 
 const button = document.querySelector('.btn');
-const content = button.dataset.content;
+const { content } = button.dataset;
 createPopover(button.dataset.content);
-
-
--- src/js/form.js
+++ src/js/form.js
@@ -1,10 +1,10 @@
-  export default function createPopover(element, content) {
-    const popover = new bootstrap.Popover(element, {
-      placement: 'top',
-      content: `${content}`,
-      template: '<div class="popover" role="tooltip">'
+export default function createPopover(element, content) {
+  const popover = new bootstrap.Popover(element, {
+    placement: 'top',
+    content: `${content}`,
+    template: '<div class="popover" role="tooltip">'
           + '<h3 class="popover-header"></h3>'
           + '<div class="popover-body"></div></div>',
-    });
-    return popover;
-  }
+  });
+  return popover;
+}
